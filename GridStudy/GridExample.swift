3050a8bb921c77f25c51036a22ad2aecb5da93db
Matheus Migge <matheusmigge@gmail.com>
2023-10-19 16:33:23 -0300

    first commit

---
+++ GridStudy/GridExample.swift
@@ -0,0 +1,20 @@
+//
+//  GridExample.swift
+//  GridStudy
+//
+//  Created by Migge on 19/10/23.
+//
+
+import SwiftUI
+
+struct GridExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GridExample_Previews: PreviewProvider {
+    static var previews: some View {
+        GridExample()
+    }
+}
