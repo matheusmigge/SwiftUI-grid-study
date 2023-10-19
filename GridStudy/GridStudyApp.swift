01003eccdade5e2383d132b059981554a11d3ce0
Matheus Migge <matheusmigge@gmail.com>
2023-10-19 16:19:44 -0300

    Initial Commit

---
+++ GridStudy/GridStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridStudyApp.swift
+//  GridStudy
+//
+//  Created by Migge on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GridStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
