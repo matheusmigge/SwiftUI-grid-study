596a2021a039d70fe6e71d06069e0855c62e3196
Matheus Migge <matheusmigge@gmail.com>
2023-10-19 16:33:30 -0300

    first commit

--- GridStudy/GridExample.swift
+++ GridStudy/GridExample.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct GridExample: View {
+    
+    let friendsList: [String] = ["Lucas, o homizão","Matheus","Fraviana","Daniboy","Bia","Marília","Berg","Anna","Bolsoni","Bruno","Piku","Agnes","Lílian","Zé","Fifizaum", "Melina", "Naná"]
+    
+    let colors: [Color] = [.red, .yellow, .blue, .green, .cyan, .pink, .orange]
+    
+    let columnLayout: [GridItem] = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columnLayout) {
+                    ForEach(0...friendsList.count-1, id: \.self) {index in
+                        ZStack {
+                            Circle()
+                                .frame(width: 170, height: 170)
+                                .foregroundColor(colors[index % colors.count])
+//                                .cornerRadius(10)
+                            
+                            VStack (spacing: 10){
+                                Image(systemName: "person.crop.circle.fill")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 40))
+                                Text("\(friendsList[index])")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 25, weight:.medium, design:.rounded))
+                            }
+                        }
+                    }
+                }
+            }
+            .padding()
+            .navigationTitle("Friend's list")
+        }
     }
 }
 
